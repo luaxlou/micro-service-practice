bbb21b997262641a502a5688274590d7951efd24
johnlou <luax@qq.com>
2017-08-27 11:50:02 +0800

    add jenkins

---
+++ seneca-listener/index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+'use strict';
+
+const Path = require('path');
+
+require('seneca')()
+    .use('..')
+    .add('cmd:salute', function(message, done) {
+        return done(null, {
+            id: Math.floor(Math.random() * (message.max - message.min + 1)) + message.min,
+            message: `Hello ${message.name}!`,
+            from: {
+                pid: process.pid,
+                file: Path.relative(process.cwd(), __filename)
+            },
+            now: Date.now()
+        });
+    })
+    .listen({
+        type: 'amqp',
+        pin: 'cmd:salute',
+        url: process.env.AMQP_URL
+    });
