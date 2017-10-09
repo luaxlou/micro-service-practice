131d25fb3f8824fd85cdfb01cd932a826b2cc66b
johnlou <luax@qq.com>
2017-10-10 07:38:29 +0800

    add env AMQP_URL

--- seneca-listener/run.sh
+++ seneca-listener/run.sh
@@ -1,2 +1,6 @@
-docker run -d -e "AMQP_URL=192.168.99.1:5672" --name seneca-listener \
+docker run \
+-d \
+-v /home/docker
+-e "AMQP_URL=192.168.99.1:5672" \
+--name seneca-listener \
 seneca-listener
